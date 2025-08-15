e04386dba1c3c02fc42645ef81d4ff35157d6005
Yikeber <yikeber50@gmail.com>
2025-08-15 11:47:25 +0300

    include slices to combined reducers to access and add them on the hook

--- frontend/src/redux/rootReducer.ts
+++ frontend/src/redux/rootReducer.ts
@@ -8,13 +8,23 @@ import projectsReducer from './slices/projectsSlice';
 import servicesReducer from './slices/servicesSlice';
 import testimonialsReducer from './slices/testimonialsSlice';
 import aboutUsReducer from './slices/aboutUsSlice';
+import teamMembersReducer from "./slices/teamMemberSlice";
+import SocialLinksReducer from "./slices/socialLinkSlice";
+import companyInfoReducer from "./slices/companyInfoSlice";
 
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  theme: themeReducer,
   users: usersReducer,
   projects:projectsReducer,
-  theme: themeReducer,
+  services: servicesReducer,
+  testimonials: testimonialsReducer,
+  aboutUs: aboutUsReducer,
+  teamMembers: teamMembersReducer,
+  socialLinks: SocialLinksReducer,
+  companyInfo: companyInfoReducer,
+  
 });
 
 export default rootReducer;
